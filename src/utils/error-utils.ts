9e88962c696d3cf44c81cfe0cc1d5872d9812582
Alexander <som17son25@gmail.com>
2023-03-29 20:05:06 +0300

    ref: create redux-utils and fix error-utils

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,17 +1,32 @@
-import { Dispatch } from 'redux'
+import {appActions} from '../features/CommonActions/App'
+import {Dispatch} from 'redux'
+import {AxiosError} from 'axios'
+import {ResponseType} from '../api/types'
 
-import { ResponseType } from 'api/todolist-api'
-import { setErrorAC, setStatusAC } from 'app/app-reducer'
-
-export const handleServerNetworkError = (dispatch: Dispatch, error: string) => {
-  dispatch(setErrorAC({ error: error }))
-  dispatch(setStatusAC({ status: 'failed' }))
+// original type:
+// BaseThunkAPI<S, E, D extends Dispatch = Dispatch, RejectedValue = undefined>
+type ThunkAPIType = {
+    dispatch: (action: any) => any
+    rejectWithValue: Function
 }
 
-export const handleServerAppError = <T>(dispatch: Dispatch, data: ResponseType<T>) => {
-  if (data.messages.length) {
-    dispatch(setErrorAC({ error: data.messages[0] }))
-  } else {
-    dispatch(setErrorAC({ error: 'Some error' }))
-  }
+export const handleAsyncServerAppError = <D>(data: ResponseType<D>,
+                                             thunkAPI: ThunkAPIType,
+                                             showError = true) => {
+    if (showError) {
+        thunkAPI.dispatch(appActions.setAppError({error: data.messages.length ? data.messages[0] : 'Some error occurred'}))
+    }
+    thunkAPI.dispatch(appActions.setAppStatus({status: 'failed'}))
+    return thunkAPI.rejectWithValue({errors: data.messages, fieldsErrors: data.fieldsErrors})
+}
+
+export const handleAsyncServerNetworkError = (error: AxiosError,
+                                              thunkAPI: ThunkAPIType,
+                                              showError = true) => {
+    if (showError) {
+        thunkAPI.dispatch(appActions.setAppError({error: error.message ? error.message : 'Some error occurred'}))
+    }
+    thunkAPI.dispatch(appActions.setAppStatus({status: 'failed'}))
+
+    return thunkAPI.rejectWithValue({errors: [error.message], fieldsErrors: undefined})
 }
